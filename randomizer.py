3d447235ef68ab12483eed9d80752c10a4042428
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-11-13 13:04:40 +0530

    Init repo

---
+++ randomizer.py
@@ -0,0 +1,8 @@
+import random
+strength = random.randint(1,5)
+x_position = random.randint(0,600);
+y_position = random.randint(0,800);
+print ("The actor with %d strength is located on the grid at {%d,%d}" 
+  % (strength, x_position, y_position))
+
+  
