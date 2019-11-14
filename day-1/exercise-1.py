500a23ba022232ad72d14d61a09a6af018db8cbf
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-11-14 09:54:52 +0530

    Refactor and added day 1 exercise

---
+++ day-1/exercise-1.py
@@ -0,0 +1,18 @@
+print("Actors", 25 + 30 / 6)
+print("Weapons", 100 - 25 * 3 % 4)
+
+# Some calculations 
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#What do you think?
+print("Is it true that 3 + 1 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+#Get comfortable with > and less than opeators
+print("Guess, Is it greater?", 5 > -3)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
