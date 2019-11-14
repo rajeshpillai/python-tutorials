a3439d27caad8d981ab97c759972dfcacd45dcc2
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-11-14 10:14:14 +0530

    Added variables exercise

---
+++ day-1/variables.py
@@ -0,0 +1,36 @@
+# Exercices to practice basic skills
+
+space_in_a_bike = 2.0  # one bike, two people
+no_of_people = 30   # total no of people
+
+# calculate no. of bikes needed
+bikes_needed =  no_of_people / space_in_a_bike
+
+print("There are", bikes_needed, 
+  "bikes needed for",no_of_people, "people")
+
+# change the no of bikes to 100
+no_of_bikes = 100
+
+# how many people can it take?
+no_of_people = no_of_bikes * 2
+
+print (no_of_bikes, "bike can take", no_of_people, "people")
+
+# count the number of tires
+total_tires = no_of_bikes * 2
+print(no_of_bikes, "bikes have", total_tires, "tires.")
+
+
+# change no of people to 10
+no_of_people = 10
+
+# amount of cash each person carries
+amount_of_cash = 50
+
+# what is the average amount?
+avg_amount = (amount_of_cash * no_of_people) / no_of_people
+
+print ("Average amount carried by ", 
+  no_of_people, " people is", avg_amount)
+
