1cb308be2b485a97df3b58d468263072ca5bcffe
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-11-16 18:38:50 +0530

    Day 2

---
+++ day-2/str-1.py
@@ -0,0 +1,25 @@
+# Hello Python
+s = 'Hello Python'
+print (s)
+print (s[0])
+
+# Get the length
+length = len(s)
+print (length)
+
+# Concatenate string
+first_name = "Nick"
+last_name = "Fury"
+
+full_name = first_name + ' ' + last_name
+
+print (full_name)
+
+# Concatenate number and a string
+
+age = 50
+
+# info = 'The age is ' + age  (throw error)
+info = 'The age is ' + str(age)
+
+print (info)
